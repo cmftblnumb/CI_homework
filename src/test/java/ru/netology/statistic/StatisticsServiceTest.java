3b9d165c2e63aed5dc74154228981f432382e6dc
Kristina P <cmftblnumb@gmail.com>
2025-01-06 12:18:44 -0500

    tests

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -22,7 +22,7 @@ class StatisticsServiceTest {
     void findMaxFromMiddle() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 30, 8, 6, 11, 11, 12};
         long expected = 30;
 
         long actual = service.findMax(incomesInBillions);
